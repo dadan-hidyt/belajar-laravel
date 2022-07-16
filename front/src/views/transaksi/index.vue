e5139ff1fd27cbc3357d30d8cb5e4cd5bed455dc
= <=>
2022-07-16 14:12:29 +0700

    dadan

--- front/src/views/transaksi/index.vue
+++ front/src/views/transaksi/index.vue
@@ -1,29 +1,59 @@
 <template>
-  <div class="index">
-    <h1>Transaksi</h1>
-    <table class="table table-bordered">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>title</th>
-          <th>Amount</th>
-          <th>Type</th>
-          <th>action</th>
+  <div class="card rounded shaddow">
+    <div class="card-header">
+      <h1>List</h1>
+    </div>
+    <div class="card-body">
+      <div class="index">
+        <table class="table table-bordered">
+          <thead>
+          <tr>
+            <th>Id</th>
+            <th>title</th>
+            <th>Amount</th>
+            <th>Type</th>
+            <th>action</th>
 
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Id</td>
-          <td>title</td>
-          <td>8933</td>
-          <td>Type</td>
-          <td>
-              <router-link to="/update/3">Update</router-link>
-             <router-link to="/update/3">Update</router-link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(transaksi, index) in transaction.data" :key="index">
+            <td>{{ transaksi.id }}</td>
+            <td>{{ transaksi.title }}</td>
+            <td>{{ transaksi.nominal }}</td>
+            <td>{{ transaksi.type }}</td>
+            <td>
+              <router-link :to="{name:'update',params:{id:transaksi.id}}" class="btn btn-sm btn-warning">Update</router-link> &nbsp;
+             <button class="btn btn-sm btn-danger" v-on:click="hapus(3)">Delete</button>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+import {onMounted,ref} from "vue";
+
+export default {
+  setup() {
+    let transaction = ref([]);
+    onMounted(()=>{
+        axios.get('http://127.0.0.1:8000/api/transaksi').then(function(e){
+           transaction.value = e.data;
+        }).catch(function(e){
+            console.log(e)
+        })
+    });
+    return {transaction};
+  },
+  methods : {
+    hapus : function(e) {
+      alert('hapus'+e)
+    }
+  }
+}
+</script>
--- front/src/views/transaksi/tambah.vue
+++ front/src/views/transaksi/tambah.vue
@@ -3,31 +3,70 @@
     <div class="card-header">
       <h1>Tambah</h1>
     </div>
-    <div class="card-body">
-      <div class="mb-3">
-        <input class="form-control" type="text" id="formFileMultiple">
+    <p v-for="error in er" class="alert alert-danger">
+      {{ error }}
+    </p>
+    <form @submit.prevent="store">
+      <div class="card-body">
+        <div class="mb-3">
+          <label for="title">title</label>
+          <input v-model="datas.title" class="form-control" type="text" id="title">
+          <span v-if="error.title">{{error.title[0]}}</span>
+        </div>
+        <div class="mb-3">
+          <label for="title">amount {{nominal}}</label>
+          <input v-model="datas.nominal" class="form-control" type="number" id="title">
+          <span v-if="error.nominal">{{error.nominal[0]}}</span>
+        </div>
+        <div class="mb-3">
+          <label for="formFileSm" class="form-label">Time</label>
+          <input v-model="datas.time" class="form-control" id="formFileSm" type="date">
+        </div>
+        <div>
+          <label for="formFileLg" class="form-label">Type</label>
+          <select class="form-control" v-model="datas.type" id="">
+            <option value="pengeluaran">pengeluaran</option>
+            <option value="pemasukan">pemasukan</option>
+
+          </select>
+          <span v-if="error.type">{{error.type[0]}}</span>
+        </div>
+        <div class="mt-3">
+          <button class="btn btn-primary">tambah</button>
+        </div>
       </div>
-      <div class="mb-3">
-        <input class="form-control" type="text" id="formFileDisabled">
-      </div>
-      <div class="mb-3">
-        <label for="formFileSm" class="form-label">Small file input example</label>
-        <input class="form-control form-control-sm" id="formFileSm" type="text">
-      </div>
-      <div>
-        <label for="formFileLg" class="form-label">Large file input example</label>
-        <input class="form-control form-control-lg" id="formFileLg" type="text">
-      </div>
-      <div class="mt-3">
-        <button class="btn btn-primary">tambah</button>
-      </div>
-    </div>
+    </form>
   </div>
 </template>
 
 <script>
+import {reactive,ref} from "vue";
+import {useRouter} from 'vue-router';
+import axios from 'axios';
 export default {
-  name: "tambah"
+setup() {
+  const datas = reactive({
+      title : '',
+      nominal : '',
+      type : '',
+  });
+  const error = ref([]);
+  const route = useRouter();
+  function store() {
+    axios.post('http://localhost:8080/api/simpan', datas,{
+      headers: {"Access-Control-Allow-Origin": "*"}
+    }).then(function(e){
+      route.push('/');
+    }).catch(function(e){
+        error.value = e.response.data;
+    })
+  }
+  return {
+    datas,
+    error,
+    store
+  }
+}
 }
 </script>
 
